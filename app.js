bc13d8c6a73774e0058494a848590056f890fd07
Dahye Min <48623630+dhmin401@users.noreply.github.com>
2023-01-01 08:41:27 +0900

    changes

--- app.js
+++ app.js
@@ -166,7 +166,7 @@ app.put("/postReply/:feedbackId/:commentId", async (req, res) => {
   }
 });
 
-app.use(express.static("client/build"));
+app.use(express.static("./client/build"));
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
