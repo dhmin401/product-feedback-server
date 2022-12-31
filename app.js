ae5bf2b9a8555120a81db418fc846c58e833833c
Dahye Min <48623630+dhmin401@users.noreply.github.com>
2023-01-01 08:13:17 +0900

    changes

--- app.js
+++ app.js
@@ -165,12 +165,10 @@ app.put("/postReply/:feedbackId/:commentId", async (req, res) => {
   }
 });
 
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(
-    path.join(path.resolve(__dirname, "./client/build"), "index.html")
-  );
+  res.sendFile(path.join(__dirname, "./client/build", "index.html"));
 });
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
